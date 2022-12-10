d13fee7d6518a670ebd3b428d457fd0953aa390e
Bartu Baş <bartubas99@gmail.com>
2022-12-10 22:53:18 +0300

    draw with color

--- src/Render.h
+++ src/Render.h
@@ -4,6 +4,7 @@
 
 #include "code_template/Color.h"
 #include "math/int2.h"
+#include "math/double3.h"
 
 using namespace std;
 
@@ -11,16 +12,18 @@ namespace Rasterizer {
 
     struct Render {
 
-        static void DrawLine(vector<vector<Color>>& output, int2 screenPos0, int2 screenPos1, Color color) {
+        static void DrawLine(vector<vector<Color>>& output, int2 screenPos0, int2 screenPos1, double3 color0, double3 color1) {
             auto x0 = screenPos0.x;
             auto y0 = screenPos0.y;
             auto x1 = screenPos1.x;
             auto y1 = screenPos1.y;
             auto d = 2 * (y0 - y1) + (x1 - x0);
             auto y = y0;
+            auto c = color0;
+            auto dc = (color1 - color0) / (x1 - x0);
 
             for (int x = x0; x <= x1; x++) {
-                DrawColor(output, int2(x, y), color);
+                DrawColor(output, int2(x, y), c);
 
                 if (d < 0) {
                     y = y + 1;
@@ -28,11 +31,13 @@ namespace Rasterizer {
                 } else {
                     d += 2 * (y0 - y1);
                 }
+
+                c = c + dc;
             }
         }
 
-        static void DrawColor(vector<vector<Color>>& output, int2 screenPos, Color color) {
-            output[screenPos.x][screenPos.y] = color;
+        static void DrawColor(vector<vector<Color>>& output, int2 screenPos, double3 color) {
+            output[screenPos.x][screenPos.y] = Color(color.x, color.y, color.z);
         }
     };
 
