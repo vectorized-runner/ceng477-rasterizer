a6517e721c40356f25be5790e92d427456c3bf7b
Bartu Baş <bartubas99@gmail.com>
2022-12-12 13:10:04 +0300

    formatting refactor

--- src/code_template/Scene.cpp
+++ src/code_template/Scene.cpp
@@ -175,260 +175,240 @@ void Scene::forwardRenderingPipeline(Camera& camera)
 /*
 	Parses XML file
 */
-Scene::Scene(const char *xmlPath)
-{
-	const char *str;
-	XMLDocument xmlDoc;
-	XMLElement *pElement;
+Scene::Scene(const char* xmlPath) {
+    const char* str;
+    XMLDocument xmlDoc;
+    XMLElement* pElement;
 
-	xmlDoc.LoadFile(xmlPath);
+    xmlDoc.LoadFile(xmlPath);
 
-	XMLNode *pRoot = xmlDoc.FirstChild();
+    XMLNode* pRoot = xmlDoc.FirstChild();
 
-	// read background color
-	pElement = pRoot->FirstChildElement("BackgroundColor");
-	str = pElement->GetText();
-	sscanf(str, "%lf %lf %lf", &backgroundColor.r, &backgroundColor.g, &backgroundColor.b);
+    // read background color
+    pElement = pRoot->FirstChildElement("BackgroundColor");
+    str = pElement->GetText();
+    sscanf(str, "%lf %lf %lf", &backgroundColor.r, &backgroundColor.g, &backgroundColor.b);
 
-	// read culling
-	pElement = pRoot->FirstChildElement("Culling");
-	if (pElement != nullptr) {
-		str = pElement->GetText();
-		
-		if (strcmp(str, "enabled") == 0) {
-			cullingEnabled = true;
-		}
-		else {
-			cullingEnabled = false;
-		}
-	}
+    // read culling
+    pElement = pRoot->FirstChildElement("Culling");
+    if (pElement != nullptr) {
+        str = pElement->GetText();
 
-	// read cameras
-	pElement = pRoot->FirstChildElement("Cameras");
-	XMLElement *pCamera = pElement->FirstChildElement("Camera");
-	XMLElement *camElement;
-	while (pCamera != nullptr)
-	{
-		Camera cam = Camera();
+        if (strcmp(str, "enabled") == 0) {
+            cullingEnabled = true;
+        } else {
+            cullingEnabled = false;
+        }
+    }
 
-		pCamera->QueryIntAttribute("id", &cam.cameraId);
+    // read cameras
+    pElement = pRoot->FirstChildElement("Cameras");
+    XMLElement* pCamera = pElement->FirstChildElement("Camera");
+    XMLElement* camElement;
+    while (pCamera != nullptr) {
+        Camera cam = Camera();
 
-		// read projection type
-		str = pCamera->Attribute("type");
+        pCamera->QueryIntAttribute("id", &cam.cameraId);
 
-		if (strcmp(str, "orthographic") == 0) {
-			cam.projectionType = 0;
-		}
-		else {
-			cam.projectionType = 1;
-		}
+        // read projection type
+        str = pCamera->Attribute("type");
 
-		camElement = pCamera->FirstChildElement("Position");
-		str = camElement->GetText();
-		sscanf(str, "%lf %lf %lf", &cam.pos.x, &cam.pos.y, &cam.pos.z);
+        if (strcmp(str, "orthographic") == 0) {
+            cam.projectionType = 0;
+        } else {
+            cam.projectionType = 1;
+        }
 
-		camElement = pCamera->FirstChildElement("Gaze");
-		str = camElement->GetText();
-		sscanf(str, "%lf %lf %lf", &cam.gaze.x, &cam.gaze.y, &cam.gaze.z);
+        camElement = pCamera->FirstChildElement("Position");
+        str = camElement->GetText();
+        sscanf(str, "%lf %lf %lf", &cam.pos.x, &cam.pos.y, &cam.pos.z);
 
-		camElement = pCamera->FirstChildElement("Up");
-		str = camElement->GetText();
-		sscanf(str, "%lf %lf %lf", &cam.v.x, &cam.v.y, &cam.v.z);
+        camElement = pCamera->FirstChildElement("Gaze");
+        str = camElement->GetText();
+        sscanf(str, "%lf %lf %lf", &cam.gaze.x, &cam.gaze.y, &cam.gaze.z);
 
-		cam.gaze = normalizeVec3(cam.gaze);
-		cam.u = crossProductVec3(cam.gaze, cam.v);
-		cam.u = normalizeVec3(cam.u);
+        camElement = pCamera->FirstChildElement("Up");
+        str = camElement->GetText();
+        sscanf(str, "%lf %lf %lf", &cam.v.x, &cam.v.y, &cam.v.z);
 
-		cam.w = inverseVec3(cam.gaze);
-		cam.v = crossProductVec3(cam.u, cam.gaze);
-		cam.v = normalizeVec3(cam.v);
+        cam.gaze = normalizeVec3(cam.gaze);
+        cam.u = crossProductVec3(cam.gaze, cam.v);
+        cam.u = normalizeVec3(cam.u);
 
-		camElement = pCamera->FirstChildElement("ImagePlane");
-		str = camElement->GetText();
-		sscanf(str, "%lf %lf %lf %lf %lf %lf %d %d",
-			   &cam.left, &cam.right, &cam.bottom, &cam.top,
-			   &cam.near, &cam.far, &cam.horRes, &cam.verRes);
+        cam.w = inverseVec3(cam.gaze);
+        cam.v = crossProductVec3(cam.u, cam.gaze);
+        cam.v = normalizeVec3(cam.v);
 
-		camElement = pCamera->FirstChildElement("OutputName");
-		str = camElement->GetText();
-		cam.outputFileName = string(str);
+        camElement = pCamera->FirstChildElement("ImagePlane");
+        str = camElement->GetText();
+        sscanf(str, "%lf %lf %lf %lf %lf %lf %d %d",
+               &cam.left, &cam.right, &cam.bottom, &cam.top,
+               &cam.near, &cam.far, &cam.horRes, &cam.verRes);
 
-		cameras.push_back(cam);
+        camElement = pCamera->FirstChildElement("OutputName");
+        str = camElement->GetText();
+        cam.outputFileName = string(str);
 
-		pCamera = pCamera->NextSiblingElement("Camera");
-	}
+        cameras.push_back(cam);
 
-	// read vertices
-	pElement = pRoot->FirstChildElement("Vertices");
-	XMLElement *pVertex = pElement->FirstChildElement("Vertex");
-	int vertexId = 1;
+        pCamera = pCamera->NextSiblingElement("Camera");
+    }
 
-	while (pVertex != nullptr)
-	{
-		Vec3 vertex = Vec3();
-		Color color = Color();
+    // read vertices
+    pElement = pRoot->FirstChildElement("Vertices");
+    XMLElement* pVertex = pElement->FirstChildElement("Vertex");
+    int vertexId = 1;
 
-		vertex.colorId = vertexId;
+    while (pVertex != nullptr) {
+        Vec3 vertex = Vec3();
+        Color color = Color();
 
-		str = pVertex->Attribute("position");
-		sscanf(str, "%lf %lf %lf", &vertex.x, &vertex.y, &vertex.z);
+        vertex.colorId = vertexId;
 
-		str = pVertex->Attribute("color");
-		sscanf(str, "%lf %lf %lf", &color.r, &color.g, &color.b);
+        str = pVertex->Attribute("position");
+        sscanf(str, "%lf %lf %lf", &vertex.x, &vertex.y, &vertex.z);
 
-		vertices.push_back(vertex);
-		colorsOfVertices.push_back(color);
+        str = pVertex->Attribute("color");
+        sscanf(str, "%lf %lf %lf", &color.r, &color.g, &color.b);
 
-		pVertex = pVertex->NextSiblingElement("Vertex");
+        vertices.push_back(vertex);
+        colorsOfVertices.push_back(color);
 
-		vertexId++;
-	}
+        pVertex = pVertex->NextSiblingElement("Vertex");
 
-	// read translations
-	pElement = pRoot->FirstChildElement("Translations");
-	XMLElement *pTranslation = pElement->FirstChildElement("Translation");
-	while (pTranslation != nullptr)
-	{
-		Translation translation = Translation();
+        vertexId++;
+    }
 
-		pTranslation->QueryIntAttribute("id", &translation.translationId);
+    // read translations
+    pElement = pRoot->FirstChildElement("Translations");
+    XMLElement* pTranslation = pElement->FirstChildElement("Translation");
+    while (pTranslation != nullptr) {
+        Translation translation = Translation();
 
-		str = pTranslation->Attribute("value");
-		sscanf(str, "%lf %lf %lf", &translation.tx, &translation.ty, &translation.tz);
+        pTranslation->QueryIntAttribute("id", &translation.translationId);
 
-		translations.push_back(translation);
+        str = pTranslation->Attribute("value");
+        sscanf(str, "%lf %lf %lf", &translation.tx, &translation.ty, &translation.tz);
 
-		pTranslation = pTranslation->NextSiblingElement("Translation");
-	}
+        translations.push_back(translation);
 
-	// read scalings
-	pElement = pRoot->FirstChildElement("Scalings");
-	XMLElement *pScaling = pElement->FirstChildElement("Scaling");
-	while (pScaling != nullptr)
-	{
-		Scaling scaling = Scaling();
+        pTranslation = pTranslation->NextSiblingElement("Translation");
+    }
 
-		pScaling->QueryIntAttribute("id", &scaling.scalingId);
-		str = pScaling->Attribute("value");
-		sscanf(str, "%lf %lf %lf", &scaling.sx, &scaling.sy, &scaling.sz);
+    // read scalings
+    pElement = pRoot->FirstChildElement("Scalings");
+    XMLElement* pScaling = pElement->FirstChildElement("Scaling");
+    while (pScaling != nullptr) {
+        Scaling scaling = Scaling();
 
-		scalings.push_back(scaling);
+        pScaling->QueryIntAttribute("id", &scaling.scalingId);
+        str = pScaling->Attribute("value");
+        sscanf(str, "%lf %lf %lf", &scaling.sx, &scaling.sy, &scaling.sz);
 
-		pScaling = pScaling->NextSiblingElement("Scaling");
-	}
+        scalings.push_back(scaling);
 
-	// read rotations
-	pElement = pRoot->FirstChildElement("Rotations");
-	XMLElement *pRotation = pElement->FirstChildElement("Rotation");
-	while (pRotation != nullptr)
-	{
-		Rotation rotation = Rotation();
+        pScaling = pScaling->NextSiblingElement("Scaling");
+    }
 
-		pRotation->QueryIntAttribute("id", &rotation.rotationId);
-		str = pRotation->Attribute("value");
-		sscanf(str, "%lf %lf %lf %lf", &rotation.angle, &rotation.ux, &rotation.uy, &rotation.uz);
+    // read rotations
+    pElement = pRoot->FirstChildElement("Rotations");
+    XMLElement* pRotation = pElement->FirstChildElement("Rotation");
+    while (pRotation != nullptr) {
+        Rotation rotation = Rotation();
 
-		rotations.push_back(rotation);
+        pRotation->QueryIntAttribute("id", &rotation.rotationId);
+        str = pRotation->Attribute("value");
+        sscanf(str, "%lf %lf %lf %lf", &rotation.angle, &rotation.ux, &rotation.uy, &rotation.uz);
 
-		pRotation = pRotation->NextSiblingElement("Rotation");
-	}
+        rotations.push_back(rotation);
 
-	// read meshes
-	pElement = pRoot->FirstChildElement("Meshes");
+        pRotation = pRotation->NextSiblingElement("Rotation");
+    }
 
-	XMLElement *pMesh = pElement->FirstChildElement("Mesh");
-	XMLElement *meshElement;
-	while (pMesh != nullptr)
-	{
-		Mesh mesh = Mesh();
+    // read meshes
+    pElement = pRoot->FirstChildElement("Meshes");
 
-		pMesh->QueryIntAttribute("id", &mesh.meshId);
+    XMLElement* pMesh = pElement->FirstChildElement("Mesh");
+    XMLElement* meshElement;
+    while (pMesh != nullptr) {
+        Mesh mesh = Mesh();
 
-		// read projection type
-		str = pMesh->Attribute("type");
+        pMesh->QueryIntAttribute("id", &mesh.meshId);
 
-		if (strcmp(str, "wireframe") == 0) {
-			mesh.type = 0;
-		}
-		else {
-			mesh.type = 1;
-		}
+        // read projection type
+        str = pMesh->Attribute("type");
 
-		// read mesh transformations
-		XMLElement *pTransformations = pMesh->FirstChildElement("Transformations");
-		XMLElement *pTransformation = pTransformations->FirstChildElement("Transformation");
+        if (strcmp(str, "wireframe") == 0) {
+            mesh.type = 0;
+        } else {
+            mesh.type = 1;
+        }
 
-		while (pTransformation != nullptr)
-		{
-			char transformationType;
-			int transformationId;
+        // read mesh transformations
+        XMLElement* pTransformations = pMesh->FirstChildElement("Transformations");
+        XMLElement* pTransformation = pTransformations->FirstChildElement("Transformation");
 
-			str = pTransformation->GetText();
-			sscanf(str, "%c %d", &transformationType, &transformationId);
+        while (pTransformation != nullptr) {
+            char transformationType;
+            int transformationId;
 
-			mesh.transformationTypes.push_back(transformationType);
-			mesh.transformationIds.push_back(transformationId);
+            str = pTransformation->GetText();
+            sscanf(str, "%c %d", &transformationType, &transformationId);
 
-			pTransformation = pTransformation->NextSiblingElement("Transformation");
-		}
+            mesh.transformationTypes.push_back(transformationType);
+            mesh.transformationIds.push_back(transformationId);
 
-		// read mesh faces
-		char *row;
-		char *clone_str;
-		int v1, v2, v3;
-		XMLElement *pFaces = pMesh->FirstChildElement("Faces");
+            pTransformation = pTransformation->NextSiblingElement("Transformation");
+        }
+
+        // read mesh faces
+        char* row;
+        char* clone_str;
+        int v1, v2, v3;
+        XMLElement* pFaces = pMesh->FirstChildElement("Faces");
         str = pFaces->GetText();
-		clone_str = strdup(str);
+        clone_str = strdup(str);
 
-		row = strtok(clone_str, "\n");
-		while (row != nullptr)
-		{
-			int result = sscanf(row, "%d %d %d", &v1, &v2, &v3);
-			
-			if (result != EOF) {
-				mesh.triangles.push_back(Triangle(v1, v2, v3));
-			}
-			row = strtok(nullptr, "\n");
-		}
-		meshes.push_back(mesh);
+        row = strtok(clone_str, "\n");
+        while (row != nullptr) {
+            int result = sscanf(row, "%d %d %d", &v1, &v2, &v3);
 
-		pMesh = pMesh->NextSiblingElement("Mesh");
-	}
+            if (result != EOF) {
+                mesh.triangles.push_back(Triangle(v1, v2, v3));
+            }
+            row = strtok(nullptr, "\n");
+        }
+        meshes.push_back(mesh);
+
+        pMesh = pMesh->NextSiblingElement("Mesh");
+    }
 }
 
 /*
 	Initializes image with background color
 */
-void Scene::initializeImage(const Camera& camera)
-{
+void Scene::initializeImage(const Camera& camera) {
     cout << "Initialize Image" << endl;
 
-	if (this->image.empty())
-	{
-		for (int i = 0; i < camera.horRes; i++)
-		{
-			vector<Color> rowOfColors;
+    if (this->image.empty()) {
+        for (int i = 0; i < camera.horRes; i++) {
+            vector<Color> rowOfColors;
 
-			for (int j = 0; j < camera.verRes; j++)
-			{
-				rowOfColors.push_back(this->backgroundColor);
-			}
+            for (int j = 0; j < camera.verRes; j++) {
+                rowOfColors.push_back(this->backgroundColor);
+            }
 
-			this->image.push_back(rowOfColors);
-		}
-	}
-	else
-	{
-		for (int i = 0; i < camera.horRes; i++)
-		{
-			for (int j = 0; j < camera.verRes; j++)
-			{
-				this->image[i][j].r = this->backgroundColor.r;
-				this->image[i][j].g = this->backgroundColor.g;
-				this->image[i][j].b = this->backgroundColor.b;
-			}
-		}
-	}
+            this->image.push_back(rowOfColors);
+        }
+    } else {
+        for (int i = 0; i < camera.horRes; i++) {
+            for (int j = 0; j < camera.verRes; j++) {
+                this->image[i][j].r = this->backgroundColor.r;
+                this->image[i][j].g = this->backgroundColor.g;
+                this->image[i][j].b = this->backgroundColor.b;
+            }
+        }
+    }
 }
 
 /*
@@ -436,44 +416,40 @@ void Scene::initializeImage(const Camera& camera)
 	If given value is more than 255, converts value to 255.
 	Otherwise returns value itself.
 */
-int Scene::makeBetweenZeroAnd255(double value)
-{
-	if (value >= 255.0)
-		return 255;
-	if (value <= 0.0)
-		return 0;
-	return (int)(value);
+int Scene::makeBetweenZeroAnd255(double value) {
+    if (value >= 255.0)
+        return 255;
+    if (value <= 0.0)
+        return 0;
+    return (int)(value);
 }
 
 /*
 	Writes contents of image (Color**) into a PPM file.
 */
-void Scene::writeImageToPPMFile(const Camera& camera)
-{
+void Scene::writeImageToPPMFile(const Camera& camera) {
     cout << "Write PPM begin" << endl;
 
-	ofstream fout;
+    ofstream fout;
 
     cout << "Output File Name: " << camera.outputFileName.c_str() << endl;
 
-	fout.open(camera.outputFileName.c_str());
+    fout.open(camera.outputFileName.c_str());
 
-	fout << "P3" << endl;
-	fout << "# " << camera.outputFileName << endl;
-	fout << camera.horRes << " " << camera.verRes << endl;
-	fout << "255" << endl;
+    fout << "P3" << endl;
+    fout << "# " << camera.outputFileName << endl;
+    fout << camera.horRes << " " << camera.verRes << endl;
+    fout << "255" << endl;
 
-	for (int j = camera.verRes - 1; j >= 0; j--)
-	{
-		for (int i = 0; i < camera.horRes; i++)
-		{
-			fout << makeBetweenZeroAnd255(this->image[i][j].r) << " "
-				 << makeBetweenZeroAnd255(this->image[i][j].g) << " "
-				 << makeBetweenZeroAnd255(this->image[i][j].b) << " ";
-		}
-		fout << endl;
-	}
-	fout.close();
+    for (int j = camera.verRes - 1; j >= 0; j--) {
+        for (int i = 0; i < camera.horRes; i++) {
+            fout << makeBetweenZeroAnd255(this->image[i][j].r) << " "
+                 << makeBetweenZeroAnd255(this->image[i][j].g) << " "
+                 << makeBetweenZeroAnd255(this->image[i][j].b) << " ";
+        }
+        fout << endl;
+    }
+    fout.close();
 }
 
 #pragma clang diagnostic pop
